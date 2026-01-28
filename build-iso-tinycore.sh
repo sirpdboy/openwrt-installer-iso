68a137675fb09ae05f4de62247db53d547c077b6
Pdboy Sir <63764630+sirpdboy@users.noreply.github.com>
2026-01-28 11:07:43 +0800

    Update build-iso-tinycore.sh

--- build-iso-tinycore.sh
+++ build-iso-tinycore.sh
@@ -14,9 +14,9 @@ NC='\033[0m'
 
 # 配置
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-INPUT_IMG="${INPUT_IMG:-${SCRIPT_DIR}/../assets/openwrt.img}"
-OUTPUT_DIR="${OUTPUT_DIR:-${SCRIPT_DIR}/../output}"
-OUTPUT_ISO_FILENAME="openwrt-installer.iso"
+INPUT_IMG="${1:-${SCRIPT_DIR}/assets/openwrt.img}"
+OUTPUT_DIR="${2:-${SCRIPT_DIR}/output}"
+OUTPUT_ISO_FILENAME=${3:-"openwrt-installer.iso"}"
 OUTPUT_ISO="${OUTPUT_DIR}/${OUTPUT_ISO_FILENAME}"
 WORK_DIR="/tmp/iso-work"
 
